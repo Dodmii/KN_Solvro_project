35bbb7a940273b9bba6b5c9d16afb1d2769c0a62
Dodmii <marcobmister@gmail.com>
2025-10-18 17:41:17 +0200

    first working typescript vue version

---
+++ src/components/FavoriteCocktails.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+      siema
+      {{ posts}}
+  </div>
+</template>
+
+<script>
+import { ref} from 'vue'
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+
+
+
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      
+      posts: ref([]),
+      error: ref(null)
+    }
+  },
+  methods:{
+      getCocktails()
+  {
+    axios('https://cocktails.solvro.pl/api/v1/cocktails')
+    .then((res) => {
+            console.log(res);
+            this.posts=res.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  }
+  },
+
+  created()
+  {
+  this.getCocktails();
+  },
+}
+
+
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
